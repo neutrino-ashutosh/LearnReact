954b6c4a35754e8b1c913b44d53eea4aa727fb6b
neutrino-ashutosh <neutrino.ashutosh@gmail.com>
2025-02-16 21:52:59 +0530

    added useRouteError and router paths

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import Header from './components/Header.js';
 import Body from './components/Body.js';
 import About from './components/About.js';
+import Contact from './components/Contact.js';
+import Error from './components/Error.js';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const AppLayout = ()=> {
@@ -17,11 +19,16 @@ const AppLayout = ()=> {
 const appRouter = createBrowserRouter([
     {
         path: "/",
-        element: <AppLayout/>
+        element: <AppLayout/>,
+        errorElement: <Error />
     },
     {
         path: "/about",
         element: <About/>
+    },
+    {
+        path: "/contact",
+        element: <Contact/>
     }
 ]);
 
---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <h3>For any queries, please reach out to us at</h3>
+    </div>
+  );
+};
+
+export default Contact;
